63e485889885c4bfaef86e316135ac2fa0436fb2
Alpha <alphamnzr@gmail.com>
2026-03-03 17:33:34 +0200

    solved errors

--- src/models/review.model.js
+++ src/models/review.model.js
@@ -15,7 +15,7 @@ const reviewSchema = new mongoose.Schema({
         min : 1,
         max : 5
     },
-    comment : { type : string},
+    comment : { type : String},
 },{timestamps : true});
 
 module.exports =  mongoose.model('Review',reviewSchema);
